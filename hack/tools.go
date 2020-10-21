26184730ca7e63b8e1d8610537fb1c10e13e5a7d
Francesco Guardiani <francescoguard@gmail.com>
2020-10-21 09:45:23 +0200

    Removed deprecated test images
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>

--- hack/tools.go
+++ hack/tools.go
@@ -35,13 +35,10 @@ import (
 
 	// Test images from eventing
 	_ "knative.dev/eventing/test/test_images/event-sender"
-	_ "knative.dev/eventing/test/test_images/filterevents"
 	_ "knative.dev/eventing/test/test_images/heartbeats"
 	_ "knative.dev/eventing/test/test_images/performance"
 	_ "knative.dev/eventing/test/test_images/print"
 	_ "knative.dev/eventing/test/test_images/recordevents"
-	_ "knative.dev/eventing/test/test_images/sequencestepper"
-	_ "knative.dev/eventing/test/test_images/transformevents"
 
 	// Licenseclassifier
 	_ "github.com/google/licenseclassifier"
