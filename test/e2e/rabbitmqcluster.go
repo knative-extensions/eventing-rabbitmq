91d801b129ff532401035de69592911401054db6
Ville Aikas <vaikas@vmware.com>
2021-01-08 13:52:56 +0200

    add more debug when rabbitmqcluster is not ready

--- test/e2e/rabbitmqcluster.go
+++ test/e2e/rabbitmqcluster.go
@@ -19,6 +19,7 @@ package e2e
 import (
 	"context"
 	"fmt"
+	"knative.dev/pkg/apis"
 
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
@@ -58,23 +59,10 @@ func RabbitMQCluster() *feature.Feature {
 }
 
 func RabbitMQClusterReady(ctx context.Context, t *testing.T) {
-	env := environment.FromContext(ctx)
-	namespace := env.Namespace()
-	rabbitCluster := corev1.ObjectReference{
-		Namespace:  namespace,
-		Name:       rabbitMQClusterName,
-		APIVersion: rabbitMQAPIVersion,
-		Kind:       rabbitMQClusterKind,
-	}
-	k := rabbitCluster.GroupVersionKind()
-	gvr, _ := meta.UnsafeGuessKindToResource(k)
-	like := &duckv1.KResource{}
-
+	namespace := environment.FromContext(ctx).Namespace()
 	lastMsg := ""
 	err := wait.PollImmediate(interval, timeout, func() (bool, error) {
-		client := dynamicclient.Get(ctx)
-
-		us, err := client.Resource(gvr).Namespace(namespace).Get(context.Background(), rabbitMQClusterName, metav1.GetOptions{})
+		conditions, err := getConditions(ctx, namespace)
 		if err != nil {
 			if apierrors.IsNotFound(err) {
 				log.Println(namespace, rabbitMQClusterName, "not found", err)
@@ -83,18 +71,12 @@ func RabbitMQClusterReady(ctx context.Context, t *testing.T) {
 			}
 			return false, err
 		}
-		obj := like.DeepCopy()
-		if err = runtime.DefaultUnstructuredConverter.FromUnstructured(us.Object, obj); err != nil {
-			log.Fatal("Error DefaultUnstructuree.Dynamiconverter ")
-		}
-		obj.ResourceVersion = gvr.Version
-		obj.APIVersion = gvr.GroupVersion().String()
 
 		// Rabbit does not have an overarching Ready condition, so spin through them all
 		// and if any is False, then do not proceed.
 		conditionCount := 0
 		allReady := true
-		for _, condition := range obj.Status.Conditions {
+		for _, condition := range conditions {
 			conditionCount++
 			if condition.Status != corev1.ConditionTrue {
 				allReady = false
@@ -109,7 +91,43 @@ func RabbitMQClusterReady(ctx context.Context, t *testing.T) {
 		return conditionCount > 0 && allReady, nil
 	})
 	if err != nil {
-		t.Fatal("RabbitMQCluster did not become ready")
+		conditions, condErr := getConditions(ctx, namespace)
+		if condErr != nil {
+			t.Errorf("Failed to get conditions for non-ready RabbitMQCluster %s/%s : %v", namespace, rabbitMQClusterName, err)
+		} else {
+			log.Printf("Conditions for non-ready RabbitMQCluster %s/%s:\n", namespace, rabbitMQClusterName)
+			for _, condition := range conditions {
+				log.Printf("%s/%s condition %s is: %s  %s: %s", namespace, rabbitMQClusterName, condition.Type, condition.Status, condition.Reason, condition.Message)
+			}
+		}
+		t.Fatalf("RabbitMQCluster %s/%s did not become ready : %v", namespace, rabbitMQClusterName, err)
 	}
 	log.Printf("rabbitmqcluster %s/%s is ready\n", namespace, rabbitMQClusterName)
+}
+
+func getConditions(ctx context.Context, namespace string) ([]apis.Condition, error) {
+	rabbitCluster := corev1.ObjectReference{
+		Namespace:  namespace,
+		Name:       rabbitMQClusterName,
+		APIVersion: rabbitMQAPIVersion,
+		Kind:       rabbitMQClusterKind,
+	}
+
+	k := rabbitCluster.GroupVersionKind()
+	gvr, _ := meta.UnsafeGuessKindToResource(k)
+
+	client := dynamicclient.Get(ctx)
+
+	like := &duckv1.KResource{}
+	us, err := client.Resource(gvr).Namespace(namespace).Get(context.Background(), rabbitMQClusterName, metav1.GetOptions{})
+	if err != nil {
+		return nil, err
+	}
+	obj := like.DeepCopy()
+	if err = runtime.DefaultUnstructuredConverter.FromUnstructured(us.Object, obj); err != nil {
+		log.Fatal("Error DefaultUnstructuree.Dynamiconverter ")
+	}
+	obj.ResourceVersion = gvr.Version
+	obj.APIVersion = gvr.GroupVersion().String()
+	return obj.Status.GetConditions(), nil
 }
