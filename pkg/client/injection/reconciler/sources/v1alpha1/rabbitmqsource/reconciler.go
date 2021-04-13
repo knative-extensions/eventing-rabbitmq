bdc1c6a68cddfc2c465a632a5a28a25f10cb3b32
Knative Automation <automation@knative.team>
2021-04-13 02:11:13 +0000

    upgrade to latest dependencies
    bumping knative.dev/pkg 3a2ae6d...b51994e:
      > b51994e Fix a few comments in reconciler-gen (# 2090)
    bumping knative.dev/eventing 4a3216c...68afe54:
      > 68afe54 update deps to latest April 12th (# 5235)
      > 515136c fix(PingSource): remove annotation based conversion logic (# 5153)
      > 39b00a6 upgrade ptypes from deprecation notices (# 5218)
    bumping knative.dev/reconciler-test 721473f...730c555:
      > 730c555 Adding Event Prober lib that wrapps eventhub into an instance. (# 173)
      > 4388d29 move eventshub to single dir (# 172)
      > 9614906 add debug to failed start pods (# 170)
      > 5218a6e upgrade to latest dependencies (# 171)
    
    Signed-off-by: Knative Automation <automation@knative.team>

--- pkg/client/injection/reconciler/sources/v1alpha1/rabbitmqsource/reconciler.go
+++ pkg/client/injection/reconciler/sources/v1alpha1/rabbitmqsource/reconciler.go
@@ -87,13 +87,13 @@ type doReconcile func(ctx context.Context, o *v1alpha1.RabbitmqSource) reconcile
 
 // reconcilerImpl implements controller.Reconciler for v1alpha1.RabbitmqSource resources.
 type reconcilerImpl struct {
-	// LeaderAwareFuncs is inlined to help us implement reconciler.LeaderAware
+	// LeaderAwareFuncs is inlined to help us implement reconciler.LeaderAware.
 	reconciler.LeaderAwareFuncs
 
 	// Client is used to write back status updates.
 	Client versioned.Interface
 
-	// Listers index properties about resources
+	// Listers index properties about resources.
 	Lister sourcesv1alpha1.RabbitmqSourceLister
 
 	// Recorder is an event recorder for recording Event resources to the
@@ -115,7 +115,7 @@ type reconcilerImpl struct {
 	skipStatusUpdates bool
 }
 
-// Check that our Reconciler implements controller.Reconciler
+// Check that our Reconciler implements controller.Reconciler.
 var _ controller.Reconciler = (*reconcilerImpl)(nil)
 
 // Check that our generated Reconciler is always LeaderAware.
