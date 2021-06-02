e026ea25027c72419ccb315fe01e21c00ee501e8
Ville Aikas <11279988+vaikas@users.noreply.github.com>
2021-06-03 01:18:44 +0300

    Webhook (#324)
    * move source webhook
    
    * add validating webhook
    
    * remove old cruft
    
    * fix copyright year
    
    * fix the error returning
    
    * handle the fact that error can be nil correctly

---
+++ cmd/webhook/broker/main.go
@@ -0,0 +1,85 @@
+/*
+Copyright 2021 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"context"
+
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	"knative.dev/pkg/configmap"
+	"knative.dev/pkg/controller"
+	"knative.dev/pkg/injection/sharedmain"
+	"knative.dev/pkg/signals"
+	"knative.dev/pkg/webhook"
+	"knative.dev/pkg/webhook/certificates"
+	"knative.dev/pkg/webhook/resourcesemantics"
+	"knative.dev/pkg/webhook/resourcesemantics/validation"
+
+	rabbitv1 "knative.dev/eventing-rabbitmq/pkg/apis/eventing/v1"
+	v1 "knative.dev/eventing/pkg/apis/eventing/v1"
+)
+
+var ourTypes = map[schema.GroupVersionKind]resourcesemantics.GenericCRD{
+	v1.SchemeGroupVersion.WithKind("Broker"): &rabbitv1.RabbitBroker{},
+}
+
+var callbacks = map[schema.GroupVersionKind]validation.Callback{
+	v1.SchemeGroupVersion.WithKind("Broker"): validation.NewCallback(rabbitv1.ValidateFunc, webhook.Create, webhook.Update),
+}
+
+func NewValidationAdmissionController(ctx context.Context, cmw configmap.Watcher) *controller.Impl {
+	// A function that infuses the context passed to ConvertTo/ConvertFrom/SetDefaults with custom metadata.
+	ctxFunc := func(ctx context.Context) context.Context {
+		return ctx
+	}
+	return validation.NewAdmissionController(ctx,
+
+		// Name of the resource webhook.
+		"validation.webhook.rabbitmq.eventing.knative.dev",
+
+		// The path on which to serve the webhook.
+		"/resource-validation",
+
+		// The resources to validate.
+		ourTypes,
+
+		// A function that infuses the context passed to Validate/SetDefaults with custom metadata.
+		ctxFunc,
+
+		// Whether to disallow unknown fields.
+		true,
+
+		// Extra validating callbacks to be applied to resources.
+		callbacks,
+	)
+}
+
+func main() {
+	// Set up a signal context with our webhook options
+	ctx := webhook.WithOptions(signals.NewContext(), webhook.Options{
+		ServiceName: "rabbitmq-broker-webhook",
+		Port:        webhook.PortFromEnv(8443),
+		// SecretName must match the name of the Secret created in the configuration.
+		SecretName: "rabbitmq-broker-webhook-certs",
+	})
+
+	sharedmain.WebhookMainWithContext(ctx, "rabbitmq-broker-webhook",
+		certificates.NewController,
+		NewValidationAdmissionController,
+	)
+}
