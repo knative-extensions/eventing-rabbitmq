6e723ef244442d048535d234d589f398c88d6d30
gabriel <gfreites@vmware.com>
2021-08-05 15:01:48 -0500

    added content type to broker producers in e2e tests

--- test/e2e/cmd/producer/main.go
+++ test/e2e/cmd/producer/main.go
@@ -41,7 +41,7 @@ func main() {
 	}
 	ctx := cloudevents.ContextWithTarget(context.Background(), env.Sink)
 
-	p, err := cloudevents.NewHTTP()
+	p, err := cloudevents.NewHTTP(cloudevents.WithHeader("Content-Type", cloudevents.ApplicationCloudEventsJSON))
 	if err != nil {
 		log.Fatal("failed to create protocol: ", err.Error())
 	}
