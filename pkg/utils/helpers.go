4ba2fb733e434d9344a4f21906512ed39ba60b66
gabriel <gfreites@vmware.com>
2022-06-03 17:25:18 -0500

    fixing e2e and unit tests

--- pkg/utils/helpers.go
+++ pkg/utils/helpers.go
@@ -20,7 +20,6 @@ import (
 	"context"
 
 	eventingduckv1 "knative.dev/eventing/pkg/apis/duck/v1"
-	"knative.dev/pkg/logging"
 )
 
 func SetBackoffPolicy(ctx context.Context, backoffPolicy string) eventingduckv1.BackoffPolicyType {
