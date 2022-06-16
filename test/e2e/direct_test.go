e32cbabf08c6051a20a30b0330b34408b5a53cb9
gabriel <gfreites@vmware.com>
2022-06-16 13:57:34 -0500

    change inter rebase

--- test/e2e/direct_test.go
+++ test/e2e/direct_test.go
@@ -22,6 +22,7 @@ package e2e
 import (
 	"context"
 
+	"knative.dev/eventing-rabbitmq/test/e2e/config/brokersecret"
 	"knative.dev/eventing-rabbitmq/test/e2e/config/brokertrigger"
 	brokerresources "knative.dev/eventing/test/rekt/resources/broker"
 	"knative.dev/reconciler-test/pkg/eventshub"
@@ -42,6 +43,37 @@ func DirectTestBroker() *feature.Feature {
 	f := new(feature.Feature)
 
 	f.Setup("install test resources", brokertrigger.Install(brokertrigger.Topology{
+		MessageCount: 5,
+		Triggers: []duckv1.KReference{
+			{
+				Kind: "Service",
+				Name: "recorder",
+			},
+		},
+	}))
+	f.Setup("RabbitMQ broker goes ready", AllGoReady)
+
+	prober := eventshub.NewProber()
+	prober.SetTargetResource(brokerresources.GVR(), "testbroker")
+	prober.SenderFullEvents(5)
+	f.Setup("install source", prober.SenderInstall("source"))
+	f.Requirement("sender is finished", prober.SenderDone("source"))
+
+	f.Alpha("RabbitMQ broker").Must("goes ready", AllGoReady)
+	f.Alpha("RabbitMQ source").
+		Must("the recorder received all sent events within the time",
+			func(ctx context.Context, t feature.T) {
+				// TODO: Use constraint matching instead of just counting number of events.
+				eventshub.StoreFromContext(ctx, "recorder").AssertAtLeast(t, 5)
+			})
+	f.Teardown("Delete feature resources", f.DeleteResources)
+	return f
+}
+
+func DirectTestBrokerConnectionSecret() *feature.Feature {
+	f := new(feature.Feature)
+
+	f.Setup("install test resources", brokersecret.Install(brokersecret.Topology{
 		MessageCount: 5,
 		Triggers: []duckv1.KReference{
 			{
