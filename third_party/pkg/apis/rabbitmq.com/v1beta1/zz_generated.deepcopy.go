04c082747cfeeedf032c5da38e9359306b01c335
gabriel <gfreites@vmware.com>
2022-11-03 20:13:43 -0500

    updaed codegen with go v1.19

--- third_party/pkg/apis/rabbitmq.com/v1beta1/zz_generated.deepcopy.go
+++ third_party/pkg/apis/rabbitmq.com/v1beta1/zz_generated.deepcopy.go
@@ -15,7 +15,7 @@ This product may include a number of subcomponents with separate copyright notic
 package v1beta1
 
 import (
-	v1 "k8s.io/api/core/v1"
+	"k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
